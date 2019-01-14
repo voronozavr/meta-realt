847f5ffb09ce37f37adccf49248bf971ab5e34dc
krauchuk <ihar.krauchuk@gmail.com>
2019-01-14 17:07:27 +0300

    fetch data from database with url params

--- server/src/parser.js
+++ server/src/parser.js
@@ -1,7 +1,7 @@
 const unirest = require('unirest');
 const cheerio = require('cheerio');
 
-module.exports.parseAds = function (url) {
+module.exports.parseAds = (url) => {
   unirest.post(url).end((response) => {
     const $ = cheerio.load(response.body);
     return $('title').text();
