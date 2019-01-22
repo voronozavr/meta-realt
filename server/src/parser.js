efa02a59d154c50bbc379d32ae42be0eb7822e74
Igor Kravchuk <voron1305@yandex.ru>
2019-01-22 09:34:11 +0300

    added scraper and parser functionality

--- server/src/parser.js
+++
@@ -1,9 +0,0 @@
-const unirest = require('unirest');
-const cheerio = require('cheerio');
-
-module.exports.parseAds = (url) => {
-  unirest.post(url).end((response) => {
-    const $ = cheerio.load(response.body);
-    return $('title').text();
-  });
-};
